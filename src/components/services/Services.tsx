2ef576336b2dc0556808decd0845ab4db0c544da
Parsa <parsa.ghorbani11@gmail.com>
2025-08-16 16:53:02 +0330

    services content updated

--- src/components/services/Services.tsx
+++ src/components/services/Services.tsx
@@ -1,7 +1,10 @@
 "use client";
 import { motion } from "motion/react";
 import React from "react";
-import { LuBug } from "react-icons/lu";
+import { AiOutlineApi } from "react-icons/ai";
+import { FaCogs } from "react-icons/fa";
+import { LuAppWindow, LuBug } from "react-icons/lu";
+import { PiDevices, PiFigmaLogo } from "react-icons/pi";
 
 const Services = () => {
   return (
@@ -25,11 +28,58 @@ const Services = () => {
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 py-16">
         <div className="flex items-start justify-center">
           <div className="p-4 rounded-full bg-sky-600 text-2xl text-white">
-            <LuBug />
+            <PiDevices />
           </div>
           <div className="px-5">
-            <h4 className="text-xl font-semibold">Lorem ipsum</h4>
-            <p className="py-3 text-sm text-gray-900">Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut iste repellat aperiam cumque! Iste explicabo, ex consequuntur aut.</p>
+            <h4 className="text-xl font-semibold">
+              Building Responsive Websites
+            </h4>
+            <p className="py-3 text-sm text-gray-900">
+              Creating websites that are accessible and function seamlessly
+              across various devices and screen sizes (desktops, tablets, and
+              mobile phones).
+            </p>
+          </div>
+        </div>
+        <div className="flex items-start justify-center">
+          <div className="p-4 rounded-full bg-sky-600 text-2xl text-white">
+            <LuAppWindow />
+          </div>
+          <div className="px-5">
+            <h4 className="text-xl font-semibold">
+              Single Page Application Development
+            </h4>
+            <p className="py-3 text-sm text-gray-900">
+              Building dynamic, interactive web applications where content
+              updates without full page reloads, providing a smoother user
+              experience
+            </p>
+          </div>
+        </div>
+        <div className="flex items-start justify-center">
+          <div className="p-4 rounded-full bg-sky-600 text-2xl text-white">
+            <PiFigmaLogo />
+          </div>
+          <div className="px-5">
+            <h4 className="text-xl font-semibold">
+              UI/UX Design Implementation
+            </h4>
+            <p className="py-3 text-sm text-gray-900">
+              Translating design mockups and wireframes into functional user
+              interfaces, considering usability and aesthetics.
+            </p>
+          </div>
+        </div>
+        <div className="flex items-start justify-center">
+          <div className="p-4 rounded-full bg-sky-600 text-2xl text-white">
+            <AiOutlineApi />
+          </div>
+          <div className="px-5">
+            <h4 className="text-xl font-semibold">API Integration</h4>
+            <p className="py-3 text-sm text-gray-900">
+              Connecting front-end interfaces with backend services and APIs to
+              fetch and display data.
+            </p>
           </div>
         </div>
         <div className="flex items-start justify-center">
@@ -37,44 +87,23 @@ const Services = () => {
             <LuBug />
           </div>
           <div className="px-5">
-            <h4 className="text-xl font-semibold">Lorem ipsum</h4>
-            <p className="py-3 text-sm text-gray-900">Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut iste repellat aperiam cumque! Iste explicabo, ex consequuntur aut.</p>
+            <h4 className="text-xl font-semibold">Testing and Debugging</h4>
+            <p className="py-3 text-sm text-gray-900">
+              Identifying and fixing bugs, ensuring websites and applications
+              function correctly across different browsers and devices.
+            </p>
           </div>
         </div>
         <div className="flex items-start justify-center">
           <div className="p-4 rounded-full bg-sky-600 text-2xl text-white">
-            <LuBug />
+            <FaCogs />
           </div>
           <div className="px-5">
-            <h4 className="text-xl font-semibold">Lorem ipsum</h4>
-            <p className="py-3 text-sm text-gray-900">Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut iste repellat aperiam cumque! Iste explicabo, ex consequuntur aut.</p>
-          </div>
-        </div>
-        <div className="flex items-start justify-center">
-          <div className="p-4 rounded-full bg-sky-600 text-2xl text-white">
-            <LuBug />
-          </div>
-          <div className="px-5">
-            <h4 className="text-xl font-semibold">Lorem ipsum</h4>
-            <p className="py-3 text-sm text-gray-900">Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut iste repellat aperiam cumque! Iste explicabo, ex consequuntur aut.</p>
-          </div>
-        </div>
-        <div className="flex items-start justify-center">
-          <div className="p-4 rounded-full bg-sky-600 text-2xl text-white">
-            <LuBug />
-          </div>
-          <div className="px-5">
-            <h4 className="text-xl font-semibold">Lorem ipsum</h4>
-            <p className="py-3 text-sm text-gray-900">Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut iste repellat aperiam cumque! Iste explicabo, ex consequuntur aut.</p>
-          </div>
-        </div>
-        <div className="flex items-start justify-center">
-          <div className="p-4 rounded-full bg-sky-600 text-2xl text-white">
-            <LuBug />
-          </div>
-          <div className="px-5">
-            <h4 className="text-xl font-semibold">Lorem ipsum</h4>
-            <p className="py-3 text-sm text-gray-900">Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut iste repellat aperiam cumque! Iste explicabo, ex consequuntur aut.</p>
+            <h4 className="text-xl font-semibold">Performance Optimization</h4>
+            <p className="py-3 text-sm text-gray-900">
+              Improving website loading times and overall performance through
+              techniques like code optimization, image compression, and caching.
+            </p>
           </div>
         </div>
       </div>
