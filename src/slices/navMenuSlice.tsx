866edab41a7231f9b7a027155f7e04adb6925964
Parsa <parsa.ghorbani11@gmail.com>
2025-09-18 19:11:38 +0330

    testimonials cleaned

--- src/slices/navMenuSlice.tsx
+++ src/slices/navMenuSlice.tsx
@@ -4,12 +4,10 @@ import { RootState } from '@/store/store';
 
 interface navState {
   isNavOpen: boolean;
-  testimonialsIndex: number
 }
 
 const initialState: navState = {
   isNavOpen: false,
-  testimonialsIndex: 0,
 };
 
 const navState = createSlice({
@@ -19,14 +17,11 @@ const navState = createSlice({
     RsetToggleNav: (state, action: PayloadAction<boolean>) => {
       state.isNavOpen = !action.payload;
     }, 
-    RsetAddIndex: (state, action: PayloadAction<number>) => {
-      state.testimonialsIndex = action.payload ;
-    },
+
   },
 });
 
-export const { RsetToggleNav , RsetAddIndex } = navState.actions;
+export const { RsetToggleNav } = navState.actions;
 export const selectToggleNav = (state: RootState) => state.nav.isNavOpen;
-export const selectAddIndex = (state: RootState) => state.nav.testimonialsIndex
 
 export default navState.reducer;
